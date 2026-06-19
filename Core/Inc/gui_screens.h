a2e6d6ad765634cb22d1d8d7a98f4e91bf8fd7dd
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-06-19 14:36:22 +0200

    update

--- Core/Inc/gui_screens.h
+++ Core/Inc/gui_screens.h
@@ -11,7 +11,17 @@
 #include "main.h"
 #include <stdint.h>
 
+typedef enum {
+    GUI_STATE_CONFIG = 0,
+    GUI_STATE_RUN,
+    GUI_STATE_CALIBRATE
+} GUI_State_t;
+
 void GUI_Init(void);
 void GUI_Task(void);
+GUI_State_t GUI_GetState(void);
+
+// Global flags
+extern volatile uint8_t gui_save_requested;
 
 #endif
